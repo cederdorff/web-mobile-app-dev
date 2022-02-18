ce224b8874299a3bbc4633ff91bdd929c2e3adc1
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-02-18 09:57:30 +0100

    Update capacitor.config.ts

--- ionic-post-app-v3/capacitor.config.ts
+++ ionic-post-app-v3/capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'post-app',
-  webDir: 'build',
-  bundledWebRuntime: false
+    appId: "io.ionic.starter",
+    appName: "post-app",
+    webDir: "build",
+    bundledWebRuntime: false
 };
 
 export default config;
