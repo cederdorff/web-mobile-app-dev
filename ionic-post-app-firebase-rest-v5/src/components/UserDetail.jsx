fe9d599bce1c5946d7279cfa2a7fc66693764eef
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-02-24 07:54:15 +0100

    Update UserDetail.jsx

--- ionic-post-app-firebase-rest-v5/src/components/UserDetail.jsx
+++ ionic-post-app-firebase-rest-v5/src/components/UserDetail.jsx
@@ -1,32 +1,21 @@
 import { IonLabel, IonAvatar, IonImg } from "@ionic/react";
-import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
-export default function UserDetail({ userId }) {
-    const [user, setUser] = useState({});
+export default function UserDetail({ user }) {
     const history = useHistory();
 
-    useEffect(() => {
-        async function loadUserData() {
-            const response = await fetch(`https://race-rest-default-rtdb.firebaseio.com/users/${userId}.json`);
-            const data = await response.json();
-            setUser(data);
-        }
-        loadUserData();
-    }, [userId]);
-
     function goToUserDetailView() {
-        history.push(`users/${userId}`);
+        history.push(`users/${user.id}`);
     }
 
     return (
         <>
             <IonAvatar slot="start" onClick={goToUserDetailView}>
-                <IonImg src={user.image} />
+                <IonImg src={user?.image} />
             </IonAvatar>
             <IonLabel onClick={goToUserDetailView}>
-                <h2>{user.name}</h2>
-                <p>{user.title}</p>
+                <h2>{user?.name}</h2>
+                <p>{user?.title}</p>
             </IonLabel>
         </>
     );
