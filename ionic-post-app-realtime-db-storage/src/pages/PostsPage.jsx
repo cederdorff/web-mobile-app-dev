9eb4569737f712fdb82a82009a073f37e72c120c
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-03-01 20:49:30 +0100

    Update PostsPage.jsx

--- ionic-post-app-realtime-db-storage/src/pages/PostsPage.jsx
+++ ionic-post-app-realtime-db-storage/src/pages/PostsPage.jsx
@@ -1,5 +1,5 @@
-import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar, useIonViewWillEnter } from "@ionic/react";
-import { useState } from "react";
+import { IonContent, IonHeader, IonList, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { useEffect, useState } from "react";
 import PostListItem from "../components/PostCard";
 import { postsRef, usersRef } from "../firebase-config";
 import { onValue, get } from "firebase/database";
@@ -7,43 +7,43 @@ import { onValue, get } from "firebase/database";
 export default function PostsPage() {
     const [posts, setPosts] = useState([]);
 
-    async function getUsers() {
-        const snapshot = await get(usersRef);
-        const usersArray = [];
-        snapshot.forEach(postSnapshot => {
-            const id = postSnapshot.key;
-            const data = postSnapshot.val();
-            const post = {
-                id,
-                ...data
-            };
-            usersArray.push(post);
-        });
-
-        return usersArray;
-    }
-
-    async function listenOnChange() {
-        const users = await getUsers();
-        onValue(postsRef, async snapshot => {
-            const postsArray = [];
+    useEffect(() => {
+        async function getUsers() {
+            const snapshot = await get(usersRef);
+            const usersArray = [];
             snapshot.forEach(postSnapshot => {
                 const id = postSnapshot.key;
                 const data = postSnapshot.val();
                 const post = {
                     id,
-                    ...data,
-                    user: users.find(user => user.id == data.uid)
+                    ...data
                 };
-                postsArray.push(post);
+                usersArray.push(post);
             });
-            setPosts(postsArray.reverse());
-        });
-    }
 
-    useIonViewWillEnter(() => {
+            return usersArray;
+        }
+
+        async function listenOnChange() {
+            const users = await getUsers();
+            onValue(postsRef, async snapshot => {
+                const postsArray = [];
+                snapshot.forEach(postSnapshot => {
+                    const id = postSnapshot.key;
+                    const data = postSnapshot.val();
+                    const post = {
+                        id,
+                        ...data,
+                        user: users.find(user => user.id == data.uid)
+                    };
+                    postsArray.push(post);
+                });
+                setPosts(postsArray.reverse());
+            });
+        }
+
         listenOnChange();
-    });
+    }, []);
 
     return (
         <IonPage className="posts-page">
