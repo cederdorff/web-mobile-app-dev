f3afcfa60c4486961a14e88c1fe6ac06ab2d083e
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-03-02 22:23:02 +0100

    ionic-push-notifications added

---
+++ ionic-push-notifications/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+    appId: "dk.eaaa.race",
+    appName: "ionic-push-notifications",
+    webDir: "build",
+    bundledWebRuntime: false
+};
+
+export default config;
---
+++ ionic-push-notifications/src/pages/Home.tsx
@@ -0,0 +1,59 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import { ActionPerformed, PushNotificationSchema, PushNotifications, Token } from "@capacitor/push-notifications";
+
+const Home: React.FC = () => {
+    console.log("Initializing HomePage");
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermissions().then(result => {
+        if (result.receive === "granted") {
+            // Register with Apple / Google to receive push via APNS/FCM
+            PushNotifications.register();
+        } else {
+            // Show some error
+        }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener("registration", (token: Token) => {
+        alert("Push registration success, token: " + token.value);
+    });
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener("registrationError", (error: any) => {
+        alert("Error on registration: " + JSON.stringify(error));
+    });
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener("pushNotificationReceived", (notification: PushNotificationSchema) => {
+        alert("Push received: " + JSON.stringify(notification));
+    });
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener("pushNotificationActionPerformed", (notification: ActionPerformed) => {
+        alert("Push action performed: " + JSON.stringify(notification));
+    });
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Push Notifications</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Push Notifications</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <ExploreContainer />
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default Home;
